77c370acced778a855f50cf79079eb61c3e735e6
Sebastian Andrykowski <swidnixx@gmail.com>
2020-11-11 16:00:02 +0100

    Dokończenie portalu

---
+++ Labirynt/Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+    float myAngle;
+
+    public void SetMyAngle(float angle)
+    {
+        myAngle = angle;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position -
+            otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        if (myAngle == 90 || myAngle == 290)
+        {
+            angularDifferenceBetweenPortalRotations -= 90;
+        }
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(
+            angularDifferenceBetweenPortalRotations, Vector3.up);
+
+        Vector3 newCameraRotation = portalRotationDifference * playerCamera.forward;
+
+        if(myAngle == 90 || myAngle == 270)
+        {
+            newCameraRotation = new Vector3(newCameraRotation.z * -1, newCameraRotation.y, newCameraRotation.x);
+            transform.rotation = Quaternion.LookRotation(newCameraRotation, Vector3.up);
+        }
+        else
+        {
+            newCameraRotation = new Vector3(newCameraRotation.x * -1, newCameraRotation.y, newCameraRotation.z);
+            transform.rotation = Quaternion.LookRotation(newCameraRotation, Vector3.up);
+        }
+    }
+}
--- Labirynt/Assets/Scripts/PortalTeleport.cs
+++ Labirynt/Assets/Scripts/PortalTeleport.cs
@@ -32,8 +32,9 @@ public class PortalTeleport : MonoBehaviour
         {
             Vector3 portalToPlayerDistance = player.position - transform.position;
             float dotProduct = Vector3.Dot(transform.up, portalToPlayerDistance);
+            
 
-            if(dotProduct < 0f)
+            if (dotProduct < 0f)
             {
                 float rotationDiff = -Quaternion.Angle(transform.rotation, recieverPortal.rotation);
                 rotationDiff += 180;
@@ -45,5 +46,16 @@ public class PortalTeleport : MonoBehaviour
 
             }
         }
+
+    }
+
+    void Update()
+    {
+        //Debug.Log("Portal: " + transform.position + " ... Docelowo: " + new Vector3(0, transform.position.y + 3, 0));
+        Debug.DrawLine(transform.position, transform.position + Vector3.up*3);
+        Debug.DrawLine(transform.position, player.position);
+        Vector3 portalToPlayerDistance = player.position - transform.position;
+        float dotProduct = Vector3.Dot(transform.up, portalToPlayerDistance);
+        Debug.Log("Dot: " + dotProduct);
     }
 }
