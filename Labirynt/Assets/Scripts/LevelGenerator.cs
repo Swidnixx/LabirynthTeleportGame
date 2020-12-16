456c171795786bc516f79ae0ff540e88cc1f9231
Sebastian Andrykowski <swidnixx@gmail.com>
2020-12-16 20:07:46 +0100

    Labirynt Generator

---
+++ Labirynt/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public ColorToPrefab[] colorMappings;
+    public Texture2D map;
+    public float offset = 5;
+
+    void Start()
+    {
+        GenerateLabirynth();
+    }
+
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+
+        if (pixelColor.a == 0)
+            return;
+
+        foreach(ColorToPrefab mapping in colorMappings)
+        {
+            if(mapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(mapping.prefab, position, Quaternion.identity);
+            }
+        }
+    }
+
+    public void GenerateLabirynth()
+    {
+        for( int x=0; x < map.width; x++)
+        {
+            for( int z=0; z < map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+    }
+
+}
